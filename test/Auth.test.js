22c2bb483f3a742507fc4dd61f75a53aad4e57e0
parakkrama24 <110714055+Parakkrama24@users.noreply.github.com>
2024-10-17 17:36:33 +0530

    test.auth

--- test/Auth.test.js
+++ test/Auth.test.js
@@ -1,1 +1,9 @@
-//test 
+process.env.NODE_ENV = "test";
+
+import { expect } from 'chai';
+
+describe('First test collection', function () {
+    it('should test two values', function () {
+        // actual test content
+    });
+});
