12ca176d53f4abb94404c140e3bf74606abf20f7
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2024-10-28 23:42:42 +0530

    Product api compelete
    add product, get product by id, get all product

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from 'express';
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js'
 dotenv.config()
 
 
@@ -34,6 +35,7 @@ app.get('/', (req, res) => {
 
 
 app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
