d491cb0a0031a3e58182ab354e5766958ab72424
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2024-10-29 15:20:41 +0530

    upadete 4 sever.js
    test

--- server.js
+++ server.js
@@ -4,43 +4,39 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
-import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js'
+dotenv.config()
 
-dotenv.config();  // Load environment variables
 
-const port = process.env.PORT || 5000;
 
-connectDB(); // Connect to MongoDB
+const port =process.env.PORT || 5000;
+
+
+connectDB(); // Connecct to MongoDB
 
 const app = express();
-
-// CORS middleware
 app.use(cors({
-  origin: 'http://localhost:5173',  // Your frontend origin
-  credentials: true,  // Allow cookies and credentials
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Allowed HTTP methods
- 
+  origin: 'https://sprojectbackend-production.up.railway.app/api/users', // Allow only the specified origin
+  methods: 'GET,POST,PUT,DELETE', // Specify allowed methods
+  allowedHeaders: 'Content-Type,Authorization' // Specify allowed headers
 }));
 
-
-// Body parser middleware
+//Body parser middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 
-// Cookie parser middleware
+//Cookie parser middleware
 app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-// API Routes
+
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 
-// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
