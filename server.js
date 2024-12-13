9b06a23921fceecfa87f4946d15f462ce837da2c
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2024-12-13 16:41:24 +0530

    add cart

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
 dotenv.config()
 
 
@@ -40,6 +41,7 @@ app.get('/', (req, res) => {
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/cart', cartRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
