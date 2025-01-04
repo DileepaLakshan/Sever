c7246fdfe24f91f1321a20e6da8c83a3b4c13ff4
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2025-01-04 12:16:36 +0530

    uri

--- server.js
+++ server.js
@@ -4,31 +4,38 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
-import productRoutes from './routes/productroutes.js';
+import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderroutes.js';
-import cartRoutes from './routes/cartroutes.js';
-dotenv.config();
+import orderRoutes from './routes/orderRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
+dotenv.config()
+
+
+const port =process.env.PORT || 5000;
+
+connectDB(); // Connecct to MongoDB
 
 const app = express();
 
-// Connect to MongoDB
-connectDB();
 
-// Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Adjust as needed
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
+  origin: 'http://localhost:5173', // Allow specific origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, // Allow cookies if needed
 }));
+
+//Body parser middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
+
+//Cookie parser middleware
 app.use(cookieParser());
 
-// Routes
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
@@ -38,5 +45,5 @@ app.use('/api/cart', cartRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-// Export the app as a Vercel serverless function
-export default app;
+
+app.listen(port, () => console.log("Server running on port ${port}"));
